813163567af66c5fc74d091a218934969f721347
jhonwii004 <jhonwii004@gmail.com>
2025-07-15 12:16:08 -0500

    frontend improvement
    Agregar color al registeer y login.

--- core/forms.py
+++ core/forms.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
-            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm',
+            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 sm:text-sm',
             'placeholder': 'Username',
         })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm',
+            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 sm:text-sm',
             'placeholder': 'Password',
         })
     )
@@ -19,7 +19,7 @@ class LoginForm(AuthenticationForm):
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
-            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 sm:text-sm',
+            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 sm:text-sm',
             'placeholder': 'Email',
         }),
         required=True
@@ -27,20 +27,20 @@ class RegisterForm(UserCreationForm):
 
     username = forms.CharField(
         widget=forms.TextInput(attrs={
-            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 sm:text-sm',
+            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 sm:text-sm',
             'placeholder': 'Username',
         })
     )
 
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 sm:text-sm',
+            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 sm:text-sm',
             'placeholder': 'Contraseña',
         })
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 sm:text-sm',
+            'class': 'appearance-none rounded-md block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 sm:text-sm',
             'placeholder': 'Confirmar contraseña',
         })
     )
