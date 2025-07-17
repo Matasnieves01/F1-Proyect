a6db925c3c4fc662ef9c9ac15d72f0c22e4be124
TroetschDaniel <danartur0307@hotmail.com>
2025-07-17 04:39:29 -0500

    chat en vivo funcionandoooo

--- infrastructure/repositories/chat_repository.py
+++ infrastructure/repositories/chat_repository.py
@@ -1,8 +1,14 @@
+import asyncio
+from asgiref.sync import sync_to_async
 from core.models import ChatMessage as ChatModel
 
-def save_chat_message(chat_entity):
+# Función síncrona original
+def save_chat_message_sync(chat_entity):
     return ChatModel.objects.create(
         user=chat_entity.user,
         content=chat_entity.content,
         timestamp=chat_entity.timestamp
     )
+
+# Función asíncrona que envuelve la síncrona
+save_chat_message = sync_to_async(save_chat_message_sync)
--- interfaces/web/routing.py
+++ interfaces/web/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
-from interfaces.web.consumers.chat_consumer import ChatConsumer
+from django.urls import path
+from .consumers.chat_consumer import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/live-chat/$", ChatConsumer.as_asgi()),
+    path('ws/chat/', ChatConsumer.as_asgi()),
 ]
